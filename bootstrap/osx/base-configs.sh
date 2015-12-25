081c2ae6c354fada6d6920abaebc94ed398f2d09
Hershal Bhave <hershal.bhave@gmail.com>
2015-12-25 15:12:10 -0600

    additional bootstrapping

--- bootstrap/osx/base-configs.sh
+++ bootstrap/osx/base-configs.sh
@@ -1,3 +1,5 @@
+git clone https://git@github.com/syl20bnr/spacemacs .emacs.d
+git clone https://git@github.com/hershal/conf.d
 source ~/conf.d/bashrc
 cd ~/
 update_links ~/conf.d/bashrc
@@ -5,3 +7,7 @@ update_links ~/conf.d/screenrc
 update_links ~/conf.d/zsh/zshrc
 update_links ~/conf.d/zsh/zprezto
 update_links ~/conf.d/zsh/zpreztorc
+update_links ~/conf.d/ssh
+
+cd ~/conf.d/
+git remote set-url origin ssh://git@github.com/hershal/conf.d
