ddc03c3dc4f1aa144d950c6e1f1feb82f0804037
Hershal Bhave <hershal.bhave@gmail.com>
2015-12-28 18:49:13 -0600

    bootstrap-osx: install cocoa emacs and coreutils

--- bootstrap/osx/brew.sh
+++ bootstrap/osx/brew.sh
@@ -1,2 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh emacs git vim carthage screen node npm wget
+brew tap homebrew/dupes
+brew install emacs --with-cocoa
+brew install zsh git vim carthage screen node node wget coreutils
