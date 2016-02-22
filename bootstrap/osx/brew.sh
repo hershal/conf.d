cc87c6a94c656c3de26cd6e1dfc93c5788a1eaa1
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-22 15:35:57 -0600

    bootstrap/osx: brightness and octave pkgs

--- bootstrap/osx/brew.sh
+++ bootstrap/osx/brew.sh
@@ -1,4 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/dupes
 brew install emacs --with-cocoa
-brew install zsh git vim carthage screen node node wget coreutils
+brew install zsh git vim carthage screen node node wget coreutils brightness octave
