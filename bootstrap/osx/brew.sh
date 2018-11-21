2bdb8089e13bc42750642327d22b036a8e19b004
Hershal Bhave <hbhave@ngvpn01-166-110.dyn.scz.us.nvidia.com>
2018-11-21 14:55:06 -0600

    Add coreutils to osx brew bootstrap

--- bootstrap/osx/brew.sh
+++ bootstrap/osx/brew.sh
@@ -1,4 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/dupes
 brew install emacs --with-cocoa
-brew install zsh git vim carthage screen wget coreutils brightness homebrew/science/octave evince bash rsync
+brew install zsh git vim carthage screen wget coreutils brightness homebrew/science/octave evince bash rsync coreutils
