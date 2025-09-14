d5f3b37533b2c7b99b6087a294ad24da7489893f
Hershal Bhave <hbhave@Avalanche.local>
2025-09-14 14:49:35 -0500

    update brew bootstrap

--- bootstrap/osx/brew.sh
+++ bootstrap/osx/brew.sh
@@ -1,4 +1,3 @@
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/dupes
-brew install emacs --with-cocoa
-brew install zsh git vim carthage screen wget coreutils brightness homebrew/science/octave evince bash rsync coreutils
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install emacs
+brew install zsh git vim carthage screen wget coreutils brightness bash rsync
